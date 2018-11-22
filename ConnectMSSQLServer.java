98f783852a55fc0af6220b1ad3d103d7fb01fab7
philipl12 <philiplin512@gmail.com>
2018-11-22 14:27:16 -0500

    Project 2 upload

--- ConnectMSSQLServer.java
+++ ConnectMSSQLServer.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class Query1
+public class ConnectMSSQLServer
 {
    public void dbConnect(
 		   String url
